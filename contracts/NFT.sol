ccc42e62a1201f09b28dff7ec652671deb3166ea
tymurpolishchuk <tymurpolishchukwork@gmail.com>
2022-09-07 14:50:51 +0300

    [contract: add requirement for mint

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,15 +6,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract NFT is ERC721, Ownable {
-    constructor(string memory name, string memory symbol)
+    address public minter;
+    constructor(string memory name, string memory symbol, address _minter)
         ERC721(name, symbol)
     {
-
+      minter = _minter;
     }
     
     mapping(uint256 => string) public ownershipRecord;
     uint256 public tokenId;
     function mintToken(address sponsor, string memory signature) public {
+        require(msg.sender == minter);
         tokenId = tokenId + 1;
         ownershipRecord[tokenId] = signature;
         _safeMint(sponsor, tokenId);
