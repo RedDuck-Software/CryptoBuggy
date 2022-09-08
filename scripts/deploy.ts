17c1d631c6e60c58042bd3844bef51fbe4e3be15
tymurpolishchuk <tymurpolishchukwork@gmail.com>
2022-09-08 14:06:47 +0300

    Added tasks

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,11 @@
+import { BigNumber, BigNumberish } from 'ethers';
 import { task } from 'hardhat/config';
 
 task('deploy-nft', 'We deploy nftf').setAction(async (_, { ethers }) => {
-  const deployer = new ethers.Wallet('', ethers.provider);
+  const deployer = new ethers.Wallet(
+    'YOUR-ADDRESS',
+    ethers.provider,
+  );
   const nftFactory = await ethers.getContractFactory('BuggyNFT', deployer);
   let _nft = await (await nftFactory.deploy('BUGGY', 'BGY')).deployed();
 
@@ -20,5 +24,31 @@ task('deploy-nft', 'We deploy nftf').setAction(async (_, { ethers }) => {
   await _nft.connect(deployer).setMinter(_buggy.address);
   await _buggyToken.connect(deployer).setMinter(_buggy.address);
 
+  console.log('NFT: ' + _nft.address);
   console.log('Buggy: ' + _buggy.address);
 });
+
+task('set-image-for-nft', 'We deploy nft').setAction(async (_, { ethers }) => {
+  const deployer = new ethers.Wallet(
+    'YOUR-ADDRESS',
+    ethers.provider,
+  );
+
+  const buggy = await ethers.getContractAt(
+    "CryptoBuggy",
+    "BUGGY-ADDRESS",
+    deployer,
+  );
+
+  const buggyNFT = await ethers.getContractAt(
+    "BuggyNFT",
+    "NFT-ADDRESS",
+    deployer,
+  );
+
+  await buggy.connect(deployer).addFund("Tymur", 1, {value: ethers.utils.parseEther('0.001')});
+  await buggyNFT.connect(deployer).setImage(1, "QmSMAt9PWkZCnhjqjvjCZTjRr72MmPhmwnAJ3pJA2epBws");
+  console.log(await buggyNFT.connect(deployer).getImage(1));
+});
+
+//await buggy.connect(deployer).addFund("Tymur", 1, {value: ethers.utils.parseEther('0.001')});
